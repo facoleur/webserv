921f2a5a729f48a3aaddd84915cee329084a026d
tgajdov <gajdov2009@gmail.com>
2025-10-13 14:00:52 +0200

    Ajout des fichiers pour config_parser

---
+++ Parser.cpp
@@ -0,0 +1,50 @@
+#include "Parser.hpp"
+#include <fstream>
+#include <sstream>
+#include <cctype>
+#include <cstdlib>
+
+ConfigParser::ConfigParser() : i_(0) {}
+
+static bool isPunct(char c)
+{
+	return c=='{' || c=='}' || c==';';
+}
+
+static bool isSpace(char c)
+{
+	return c==' ' || c=='\t' || c=='\r' || c=='\n' || c=='\v' || c=='\f';
+}
+
+std::vector<ConfigParser::Token> ConfigParser::tokenize(const std::string& text)
+{}
+
+bool ConfigParser::isMethod(const std::string& m)
+{
+	return m == "GET" || m == "POST" || m == "DELETE";
+}
+
+int ConfigParser::toInt(const std::string& s)
+{
+	// minimal, ne check pas overflow
+	return std::atoi(s.c_str());
+}
+
+Config ConfigParser::parseFile(const std::string& path)
+{
+
+}
+
+Config ConfigParser::parseString(const std::string& text)
+{
+}
+
+void ConfigParser::parseConfig()
+{
+
+}
+
+void ConfigParser::parseServer()
+{
+
+}
